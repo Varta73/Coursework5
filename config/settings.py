2026685fe16033b51c8e99a3523bdca8f9afa0bd
Svetlana <varta.73@mail.ru>
2025-04-21 00:54:44 +0300

    Инициализация проекта

---
+++ config/settings.py
@@ -0,0 +1,151 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG", False) == "True"
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_yasg",
+    "rest_framework_simplejwt",
+    "django_celery_beat",
+    "corsheaders",
+    "users",
+    "habits",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT", default="5432"),
+    }
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = "static/"
+
+AUTH_USER_MODEL = "users.User"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "UPDATE_LAST_LOGIN": True,
+}
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://127.0.0.1:8000",
+]
+CORS_ALLOW_ALL_ORIGINS = False
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "UPDATE_LAST_LOGIN": True,
+}
+
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+TELEGRAM_URL = "https://api.telegram.org/bot"
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_BROKER_URL")
+
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+CELERY_BEAT_SCHEDULE = {
+    "send_habit_reminder": {
+        "task": "habits.tasks.send_habit_reminder",
+        "schedule": timedelta(minutes=1),
+    },
+}
---
+++ habits/tasks.py
@@ -0,0 +1,70 @@
+import datetime
+from datetime import timedelta
+
+import pytz
+import requests
+from celery import shared_task
+from django.utils import timezone
+
+from config import settings
+from habits.models import Habit
+
+
+@shared_task
+def send_telegram_message(chat_id, message):
+    """Функция отправки уведомления в телеграм."""
+
+    params = {
+        "chat_id": chat_id,
+        "text": message,
+    }
+    requests.get(f"{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage", params=params)
+
+
+@shared_task
+def send_habit_reminder():
+    """Отправляет напоминания пользователю в телеграм о необходимости выполнить полезную привычку
+    за 5 мин до ее начала, в момент выполнения, а также напоминание о приятной привычке или вознаграждении."""
+
+    zone = pytz.timezone(settings.TIME_ZONE)
+    current_datetime = datetime.datetime.now(zone)
+
+    habits = Habit.objects.filter(
+        do_at__lte=current_datetime + timedelta(minutes=5),
+        do_at__gt=current_datetime,
+        is_enjoyable=False,
+    )
+    if habits:
+        for habit in habits:
+            message_1 = (f"У вас запланирована привычка: "
+                         f"{habit.user.first_name if habit.user.first_name else habit.user.email}, "
+                         f"через 5 минут пора {habit.action} {habit.place}!"
+                         f" ({timezone.localtime(habit.do_at).strftime("%d.%m.%Y %H:%M")})")
+            message_2 = f"Время {habit.action}!"
+            if habit.user.tg_chat_id:
+                # Отправка напоминания за 5 мин до начала привычки
+                send_telegram_message(habit.user.tg_chat_id, message_1)
+                habit.do_at += timedelta(days=habit.periodicity)
+                habit.save()
+
+                # Отправка напоминания о начале привычки
+                send_telegram_message.apply_async(args=[habit.user.tg_chat_id, message_2], countdown=60)
+
+                # Отправка напоминания о выполнении приятной привычки при ее наличии
+                if habit.related_habit:
+                    enjoyable_habit = habit.related_habit
+                    message = f"Вы молодец! Настало время сделать что-то приятное. Пора {enjoyable_habit.action}!"
+                    send_telegram_message.apply_async(
+                        args=[habit.user.tg_chat_id, message],
+                        countdown=(habit.duration + 5 * 60),
+                    )
+
+                # Отправка напоминания о вознаграждении при его наличии
+                else:
+                    message = f"Вы молодец! Настало время получить вознаграждение. Пора {habit.reward}!"
+                    send_telegram_message.apply_async(
+                        args=[habit.user.tg_chat_id, message],
+                        countdown=(habit.duration + 5 * 60),
+                    )
+            else:
+                print("Укажите свой tg_chat_id для рассылки напоминаний")
